a7420f398d61b8f698a10ffd5fda0ec6db0191ce
BlackCode777 <blackcode77.1980@gmail.com>
2023-05-31 10:11:28 -0300

    entendendo event binding ...

--- estudos_angular/EstudosAngular-cursos/src/app/event-binding-test/event-binding-test.component.ts
+++ estudos_angular/EstudosAngular-cursos/src/app/event-binding-test/event-binding-test.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-binding-test.component.css']
 })
 export class EventBindingTestComponent {
+        
+        public position: {x: number, y:number} = {x: 0, y:0}
+
+        constructor(){}
+        ngOnInit(){}
+
+        public mouseMoveT(valor: MouseEvent){
+                console.log(valor)
+                this.position.x  = valor.offsetX
+                this.position.y = valor.offsetY
+        }
+
+        public alertaInfo(valor: MouseEvent){
+                console.log(valor)
+        }
+
+        // public alertaInfo(valor: string){
+        //         alert("Pegando eventos com event-binding")
+        // }
 
 }

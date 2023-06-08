fade035a7ae910953941898b1c130d2b84c4810f
BlackCode777 <blackcode77.1980@gmail.com>
2023-06-08 14:34:30 -0300

    Completando estudo do ngModel

--- estudos_angular/EstudosAngular-cursos/src/app/estudo-ng-model/estudo-ng-model.component.ts
+++ estudos_angular/EstudosAngular-cursos/src/app/estudo-ng-model/estudo-ng-model.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./estudo-ng-model.component.css']
 })
 export class EstudoNgModelComponent {
+        
+        public valor: boolean = false; // Variavel ngClass
+        public heigthPX: string = '20px'; // Variavel ngStyle
+        public fontS: string = '30px'
+        public background: string = '';
+
+        public nome: string = ''
+        public listNome: Array<{nome: string}> = []; // dessa forma se manipula listas 
+
+        constructor(){}
+        ngOnInit(){
+                this.trocaValorNgClass();
+        }
+
+        public salvar(){
+                this.listNome.push({ nome: this.nome }); // Aqui cria um objeto lista e usa o push pra monstrar ele 
+                this.nome = "";
+        }
+
+        public trocaValorNgClass(){
+                setInterval( () => {
+                        /* Manipulando NgClass */
+                        if(this.valor && this.valor != null && this.valor != undefined){
+                                this.valor = false;
+                        } else {
+                                this.valor = true;
+                        }
+                        /* Manipulando NgStyle */
+                        if( this.heigthPX == '20px' && this.heigthPX != null && this.heigthPX != undefined){
+                                this.heigthPX = '50px';
+                                this.background = '#d400ff';
+                        }else{
+                                this.heigthPX = '20px';
+                                this.background = '#c8ff00';
+                        }
+                }, 2000 );
+        }
 
 }

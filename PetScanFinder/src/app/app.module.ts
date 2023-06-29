ad245180279ce9e1ba27f181844724928515da34
Anderson Martins <BlackCode77.1980@gmail.com>
2023-06-29 05:24:39 -0300

    ...

--- PetScanFinder/src/app/app.module.ts
+++ PetScanFinder/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import QuaggaModule 
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // QuaggaModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot(AppRoutingModule.routes),
     BrowserModule,
     AppRoutingModule
   ],

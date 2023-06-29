c6ceff6c63f84b64a1e3c3e1fb8c43d852cda4f7
Anderson Martins <BlackCode77.1980@gmail.com>
2023-06-29 05:23:46 -0300

    ...

--- PetScanFinder/src/app/app-routing.module.ts
+++ PetScanFinder/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'camera', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  static routes: Routes;
+}
--- PetScanFinder/src/app/app.component.ts
+++ PetScanFinder/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import Quagga from 'quagga';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template:
+  `
+  <div #cameraView></div>
+  <button (click)="capture()">Take Picture</button>
+  `
+  
 })
 export class AppComponent {
-  title = 'PetScanFinder';
+  
+  @ViewChild('cameraView')
+  cameraView!: ElementRef;
+  onDetected: any;
+  onDetectedError: any;
+
+  construtor() {}
+
+  // crie uma função para capturar a imagem
+  capture() { 
+    // crie uma variável para acessar o elemento nativo
+    const video = this.cameraView.nativeElement;
+
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: video
+      },
+      decoder: {
+        readers: ["ean_reader"] //Configura o tipo de leitura do código de barras
+      }
+    }, (err: any) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+      Quagga.onDetected(this.onDetected.bind(this));
+      Quagga.onDetectedError(this.onDetectedError.bind(this));      
+      Quagga.stop();
+    });
+
+    // crie uma variável para acessar a câmera
+    // navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+    //   .then(stream => video.srcObject = stream)
+    //   .catch(error => console.log(error));
+  }
+
 }

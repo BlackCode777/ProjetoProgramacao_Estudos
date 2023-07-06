42b31b189c7a27ab531619034c33d78c94b5cb87
Anderson Martins <BlackCode77.1980@gmail.com>
2023-07-06 00:12:30 -0300

    [ Java ] - Orientação POO com

---
+++ PaulDeitel_ESTUDOLIVRO/estudo_livro_paul_deitel/src/main/java/com/estudo_1/ObjetosJava/OrientacaoObjetosJava.java
@@ -0,0 +1,55 @@
+
+package com.estudo_1.ObjetosJava;
+
+public class OrientacaoObjetosJava {
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+
+    OrientacaoObjetosJava() {
+    };
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return int return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
